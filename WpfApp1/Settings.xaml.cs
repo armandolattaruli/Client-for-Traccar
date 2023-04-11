8f12b7538f866be2fc6cb031e8d7db1e9c323917
armandolattaruli <armandolattaruli@gmail.com>
2023-04-11 15:38:53 +0200

    Minor adjustments.

--- WpfApp1/Settings.xaml.cs
+++ WpfApp1/Settings.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApp1
         public Settings()
         {
             InitializeComponent();
-            //loadLinkServer();
+            loadLinkServer();
         }
 
         private void loadLinkServer()
