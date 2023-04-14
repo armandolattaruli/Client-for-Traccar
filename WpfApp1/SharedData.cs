cb3a8f6b16727f6f3a67921ce2012bb6b1ff932e
armandolattaruli <armandolattaruli@gmail.com>
2023-04-14 13:22:58 +0200

    Fixing graphical infos when task is being executed

--- WpfApp1/SharedData.cs
+++ WpfApp1/SharedData.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Client_for_Traccar
 {
     public static class SharedData
     {
         public static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        public static void createTask()
+        public static void createTask(MainWindow mainWindow)
         {
             Task.Run(async () =>
             {
@@ -20,9 +22,18 @@ namespace Client_for_Traccar
                     Client_for_Traccar.GeoFinder.findPosition();
                     GeoFinder.magicClient();
 
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        mainWindow.textForLatitude.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF2CB9F5");
+                        mainWindow.textForLongitude.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF2CB9F5");
+                        mainWindow.dateUpdate.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF2CB9F5");
+                        mainWindow.textForLatitude.Text = GeoFinder.getLatitude();
+                        mainWindow.textForLongitude.Text = GeoFinder.getLongitude();
+                        mainWindow.dateUpdate.Text = DateTime.Now.ToString();
+                    });
 
-                    // Attendi per 5 secondi prima di eseguire nuovamente il codice
-                    await Task.Delay(TimeSpan.FromSeconds(10));
+                    // Wait 10 seconds before sending position again
+                    await Task.Delay(TimeSpan.FromSeconds(Properties.Settings.Default.connectionTimeOut));
                 }
             }, _cancellationTokenSource.Token);
         }
